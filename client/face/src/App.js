b56fb356a1ddd5c969c8ef8d6ee3a8c03d3166b3
Shazwi Suwandi <shazwisuwandi@shazwis-air.lan>
2022-06-25 19:56:03 +0800

    added tailwind ui

--- client/face/src/App.js
+++ client/face/src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <p className="emoji text-center">😉</p>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Beauty Score v1.0</h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Upload a face portrait and determine a beauty score.
+          </p>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" action="#" method="POST">
+              <div className="max-w-xl">
+                  <label
+                      className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+                      <span className="flex items-center space-x-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24"
+                              stroke="currentColor" stroke-width="2">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                          </svg>
+                          <span className="font-medium text-gray-600">
+                              <span className="text-blue-600 underline">Upload a file</span> or drag and drop
+                          </span>
+                      </span>
+                      <input type="file" name="file_upload" className="hidden" />
+                  </label>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                >
+                  Give me a score
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
   );
 }
-
-export default App;
